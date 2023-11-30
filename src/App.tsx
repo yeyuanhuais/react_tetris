931ecac8b111b3d281c138b0c50011f50168def3
yeyuanhua <1378476425@qq.com>
2023-11-30 23:08:18 +0800

    页面聚焦

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "@/App.css";
 import { useAppDispatch, useAppSelector } from "@/hook/storeHook";
 import { StoreReducer } from "@/store";
 import { useResize } from "@/hook/useResize";
+import { isFocus, visibilityChangeEvent } from "./unit";
 
 function App() {
   const [count, setCount] = useState(0);
   const music = useAppSelector((state: StoreReducer) => state.music.value);
   const dispatch = useAppDispatch();
   const [width, height] = useResize();
-  return (
-    <>
-    </>
-  );
+  useEffect(() => {
+    if (visibilityChangeEvent) {
+      document.addEventListener(
+        visibilityChangeEvent,
+        () => {
+          dispatch("", isFocus());
+        },
+        false
+      );
+    }
+    return document.removeEventListener(
+      visibilityChangeEvent,
+      () => {
+        dispatch("", isFocus());
+      },
+      false
+    );
+  }, []);
+  return <></>;
 }
 
 export default App;
--- src/unit/index.ts
+++ src/unit/index.ts
@@ -3,13 +3,22 @@
  */
 const hiddenProperty = (() => {
   let names = ["hidden", "webkitHidden", "mozHidden", "msHidden"];
-  names = names.filter(e => e in document);
+  names = names.filter((e) => e in document);
   return names.length > 0 ? names[0] : false;
 })();
-const visibilityChangeEvent = (() => {
+export const visibilityChangeEvent = (() => {
   if (!hiddenProperty) {
     return false;
   }
   // 如果属性有前缀, 相应的事件也有前缀
   return hiddenProperty.replace(/hidden/i, "visibilitychange");
 })();
+export const isFocus = () => {
+  // 如果不存在该特性，认为一直聚焦
+  if (!hiddenProperty) {
+    return true;
+  }
+  return !document[hiddenProperty];
+};
+const unit = () => {};
+module.exports = unit;
